692814332d7be0663637d9d4d26c79a549873d05
dillipkm <dillip331@gmail.com>
2016-07-22 11:07:45 +0530

    Update app file

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ testApp.controller("formCtrl", function($scope){
   $scope.submitForm = function(){
     $scope.customer = {firstName:$scope.firstName, lastName:$scope.lastName, email:$scope.email, country:$scope.country};
     console.log($scope.customer);
-  }
+  };
 });
